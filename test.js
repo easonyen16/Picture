9b2ea3a4f468d0a48282668165820020a733b99f
easonyen16 <eason990024@gmail.com>
2021-08-05 22:07:54 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,15 +6,15 @@ let picLinks = source.split('\n');
 picLinks = picLinks.filter((x)=>{
     return x.trim();
 })
-div += '<figure class="wp-block-gallery columns-3 is-cropped">\r\n';
-div += '	<ul class="blocks-gallery-grid">\r\n';
+div += '&lt;figure class="wp-block-gallery columns-3 is-cropped">\r\n';
+div += '	&lt;ul class="blocks-gallery-grid">\r\n';
 for (let i = 0;i&lt;picLinks.length;i++){
-	div += '		<li class="blocks-gallery-item">\r\n';
-	div += '			<figure>\r\n';
-	div += '				<img alt="" class="wp-image-534" data-link="'+picLinks[i].trim()+'" src="'+picLinks[i].trim()+'?raw=true">\r\n';
-	div += '			</figure>\r\n';
-	div += '		</li>\r\n';
+	div += '		&lt;li class="blocks-gallery-item">\r\n';
+	div += '			&lt;figure>\r\n';
+	div += '				&lt;img alt="" class="wp-image-534" data-link="'+picLinks[i].trim()+'" src="'+picLinks[i].trim()+'?raw=true">\r\n';
+	div += '			&lt;/figure>\r\n';
+	div += '		&lt;/li>\r\n';
 }
-div += '	</ul>';
-div += '</figure>';	
+div += '	&lt;/ul>';
+div += '&lt;/figure>';	
 targetEle.value = div;
