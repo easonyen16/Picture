d3b28d8c487353916a5179c4ba7d4ee752c970ea
easonyen16 <eason990024@gmail.com>
2021-08-07 12:38:19 +0800

    Update test.js

--- test.js
+++ test.js
@@ -3,18 +3,19 @@ var targetEle = document.getElementById('target');
 var source = sourceEle.value;
 let div = '';
 let picLinks = source.split('\n');
+picLinks.sort();
 picLinks = picLinks.filter((x)=>{
     return x.trim();
 })
-div += '&lt;figure class="wp-block-gallery columns-3 is-cropped">\r\n';
-div += '	&lt;ul class="blocks-gallery-grid">\r\n';
-for (let i = 0;i&lt;picLinks.length;i++){
-	div += '		&lt;li class="blocks-gallery-item">\r\n';
-	div += '			&lt;figure>\r\n';
-	div += '				&lt;img alt="" class="wp-image-534" data-link="'+picLinks[i].trim()+'" src="'+picLinks[i].trim()+'?raw=true">\r\n';
-	div += '			&lt;/figure>\r\n';
-	div += '		&lt;/li>\r\n';
+div += '<figure class="wp-block-gallery columns-3 is-cropped">\r\n';
+div += '	<ul class="blocks-gallery-grid">\r\n';
+for (let i = 0;i<picLinks.length;i++){
+	div += '		<li class="blocks-gallery-item">\r\n';
+	div += '			<figure>\r\n';
+	div += '				<img alt="" class="wp-image-534" data-link="'+picLinks[i].trim()+'" src="'+picLinks[i].trim()+'?raw=true">\r\n';
+	div += '			</figure>\r\n';
+	div += '		</li>\r\n';
 }
-div += '	&lt;/ul>';
-div += '&lt;/figure>';	
+div += '	</ul>';
+div += '</figure>';	
 targetEle.value = div;
